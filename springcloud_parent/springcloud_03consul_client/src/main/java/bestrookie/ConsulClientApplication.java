fd589afe4eee86923a3b983206bdc2b32597fa0f
bestrookie <491051587@qq.com>
2021-08-03 18:03:08 +0800

    ..

---
+++ springcloud_parent/springcloud_03consul_client/src/main/java/bestrookie/ConsulClientApplication.java
@@ -0,0 +1,13 @@
+package bestrookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author bestrookie
+ * @date 2021/8/3 6:00 下午
+ */
+@SpringBootApplication
+public class ConsulClientApplication {
+
+}
