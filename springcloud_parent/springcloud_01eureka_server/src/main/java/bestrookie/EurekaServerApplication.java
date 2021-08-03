81ddcd68f708d1488fe22db669fa1659fd4f8101
bestrookie <491051587@qq.com>
2021-08-03 17:36:46 +0800

    Initial commit

---
+++ springcloud_parent/springcloud_01eureka_server/src/main/java/bestrookie/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package bestrookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author bestrookie
+ * @date 2021/8/2 10:25 上午
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
---
+++ springcloud_parent/springcloud_02eureka_client/src/main/java/bestrookie/EurekaClientApplication.java
@@ -0,0 +1,17 @@
+package bestrookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author bestrookie
+ * @date 2021/8/2 3:57 下午
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class,args);
+    }
+}
